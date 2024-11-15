638f239c6636076b9fa6dcb46213e5866477ffe1
Juliana Hall <juju.azeredohall@gmail.com>
2024-11-14 21:43:24 -0300

    musica

--- score_label.gd
+++ score_label.gd
@@ -1,8 +1,14 @@
 extends Label
 
 var score = 0000
+var som_moeda: AudioStreamPlayer  # Referência ao player de som de moeda
 
 func _ready():
+	# Cria o player de som e adiciona à cena
+	som_moeda = AudioStreamPlayer.new()
+	add_child(som_moeda)
+	# Carrega o arquivo de som para o player
+	som_moeda.stream = load("res://Musica/collect points.mp3")
 	# Conecta o sinal 'moeda_coletada' de cada moeda na cena ao ScoreLabel
 	for moeda in get_tree().get_nodes_in_group("Moedas"):
 		moeda.connect("moeda_coletada", Callable(self, "_on_moeda_coletada"))
@@ -10,3 +16,5 @@ func _ready():
 func _on_moeda_coletada():
 	score += 1  # Incrementa a pontuação
 	text = "Score: " + str(score)
+	# Toca o som da moeda
+	som_moeda.play()
