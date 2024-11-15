b749536d98d69df0662cd3156cd2b5c1ca09a24f
Juliana Hall <juju.azeredohall@gmail.com>
2024-11-14 22:26:55 -0300

    prox fase

--- area_2d.gd
+++ area_2d.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
-func _on_Area2D_body_entered(body):
-	if body.name == "CollisionShape2D":  # Verifica se o objeto que entrou na área é o personagem
-		get_tree().change_scene_to_file("res://fase_2.tscn")
+func _ready() -> void:
+	await get_tree().create_timer(2.0).timeout  # Aguarda 2 segundos
+	get_tree().change_scene_to_file("res://fase_2.tscn")
